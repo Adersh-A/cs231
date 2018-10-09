262732c0265b7e684cf757343c82210d3f857ec8
ceccs17d02 <43125084+ceccs17d02@users.noreply.github.com>
2018-10-09 15:37:09 +0530

    Add files via upload

---
+++ insort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int inSort(int array[], int n) {
+	int i, j, temp;
+	for (i=1; i<n; i++) {
+		temp = array[i];
+		for (j=i-1; j>=0 && array[j] > temp; j--) {
+				array[j+1] = array[j];
+		}
+		array[j+1] = temp;
+	}
+}
+void main() {
+	int array[100], i, j, size;
+
+	printf("Enter size of the array : ");
+	scanf("%d", &size);
+
+	printf("Enter array\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	inSort(array, size);
+	
+	printf("Sorted array\n");
+	for (i=0; i<size; i++) {
+		printf("%d\n", array[i]);
+	}
+}

f2a1bf6d18a7c8d5da8ae33a9a734e379d75d56d
ceccs17d02 <43125084+ceccs17d02@users.noreply.github.com>
2018-10-09 15:27:47 +0530

    Add files via upload

---
+++ selsort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int selSort(int array[], int n) {
+	int i, j, pos, temp;
+	for (i=0; i<n; i++) {
+		pos = i;
+
+		for (j=i; j<n; j++) {
+			if (array[j] < array[pos]) {
+				pos =j;
+			}
+		}
+		if (i != pos) {
+			temp = array[i];
+			array[i] = array[pos];
+			array[pos] = temp;
+		}
+	}
+}
+
+int main() {
+	int i, j, pos, array[100], temp, size;
+
+	printf("Enter number of elements in the array : ");
+	scanf("%d", &size);
+
+	printf("Enter the array\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+	
+	selSort(array, size);
+
+	printf("Sorted array \n");
+	for (i=0; i<size; i++) {
+		printf("%d\n", array[i]);
+	}
+}

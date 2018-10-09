b4c4c5a092cb0310083c32d9689fbe2d7fef5e23
ceccs17d02 <43125084+ceccs17d02@users.noreply.github.com>
2018-10-09 15:21:25 +0530

    added function

---
+++ seqsearch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int seqSearch(int array[], int n, int ele) {
+	int i;
+
+	for (i=0; i<n; i++) {
+		if (array[i] == ele) {
+			return (i+1);
+		}
+	}
+	return (0);
+}
+int main() {
+	int i=0, flag=0, array[100], size, element;
+
+	printf("Enter number of elements : ");
+	scanf("%d", &size);
+
+	printf("Enter elements\n");
+	for (i=0; i<size; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	printf("Enter element to search : ");
+	scanf("%d", &element);
+
+	flag = seqSearch(array, size, element);
+
+	if (flag) {
+		printf("Element found at position %d\n", flag);
+	} else {
+		printf("Element not found\n");
+	}
+}
